0a5238da9f4c63092e4bef5c4946db954482af67
VanceGoblin <zvkubajak@gmail.com>
2025-01-13 16:45:59 -0600

    Created Other User Operations

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -4,8 +4,9 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
+const saltRounds = 10;
 
-export const getAllUsers = async (_req: Request, res: Response) => {
+export const getUsers = async (_req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany({
       select: {
@@ -24,7 +25,7 @@ export const getAllUsers = async (_req: Request, res: Response) => {
   }
 };
 
-export const getUser = async (req: Request, res: Response) => {
+export const getUserById = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
@@ -48,6 +49,126 @@ export const getUser = async (req: Request, res: Response) => {
     }
   } catch (error: any) {
     console.error("Error fetching user by id:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+
+  try {
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ message: "Invalid email. Please provide a valid email." });
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({
+        message:
+          "Invalid password. Password must be 8-20 characters long, include at least one lowercase letter, one uppercase letter, one number, and one special character.",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = await prisma.user.create({
+      data: { username, email, password: hashedPassword },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        myQuotes: true,
+        createdAt: true,
+      },
+    });
+
+    res.status(201).json({ message: "User created successfully.", user });
+  } catch (error: any) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { username, email, password, myQuotes } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const updateData: any = {};
+
+    if (username) updateData.username = username;
+
+    if (email) {
+      if (!validator.isEmail(email)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid email. Please provide a valid email." });
+      } else {
+        updateData.email = email;
+      }
+    }
+
+    if (password) {
+      if (!validator.isStrongPassword(password)) {
+        return res.status(400).json({
+          message:
+            "Invalid password. Password must be 8-20 characters long, include at least one lowercase letter, one uppercase letter, one number, and one special character.",
+        });
+      } else {
+        updateData.password = await bcrypt.hash(password, saltRounds);
+      }
+    }
+
+    if (myQuotes) updateData.myQuotes = myQuotes;
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: updateData,
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        myQuotes: true,
+        createdAt: true,
+      },
+    });
+
+    res
+      .status(200)
+      .json({ message: "User updated successfully.", updatedUser });
+  } catch (error: any) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (user) {
+      await prisma.user.delete({
+        where: { id },
+      });
+      res.status(200).json({ message: "User deleted successfully." });
+    } else {
+      res.status(404).json({ message: "User not found." });
+    }
+  } catch (error: any) {
+    console.error("Error deleting user:", error);
     res.status(500).json({ message: error.message });
   }
 };
