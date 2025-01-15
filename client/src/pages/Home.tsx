02e1046c44090be80b42d6aac9c3be8afd26cc80
VanceGoblin <zvkubajak@gmail.com>
2025-01-14 20:35:44 -0600

    Added Caveat Font to Tailwind Config

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,11 +3,13 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
     <main>
-      <section className="heading">
-        <h1>Quoteful</h1>
-        <p>Explore and create your own thoughtful quotes.</p>
+      <section id="heading">
+        <h1 className="font-mono">Quoteful</h1>
+        <p className="font-caveat">
+          Explore and create your own thoughtful quotes.
+        </p>
 
-        <div className="heading-cta-buttons">
+        <div id="heading-cta-buttons">
           <Button>Explore</Button>
           <button className="bg-red-500 text-white p-4">Create a Quote</button>
         </div>
