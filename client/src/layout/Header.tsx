2f1ef795ae957243351c8368159d8ab2c8b8b155
VanceGoblin <zvkubajak@gmail.com>
2025-01-14 22:26:13 -0600

    Created Header.tsx

---
+++ client/src/layout/Header.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { Quote } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "../components/ui/navigation-menu";
+
+const Header = () => {
+
+}
+
+export default Header;
