7267b710935e1a876ab85bdeca693fdace81bbf6
VanceGoblin <zvkubajak@gmail.com>
2025-01-15 09:09:10 -0600

    Added Transitions to Links

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -20,32 +20,32 @@ const Header = () => {
       >
         <Link
           to="/explore"
-          className={`text-gray-400 hover:text-white ${
-            currentPage === "/explore" ? "text-white-400 font-bold" : ""
+          className={`text-gray-400 hover:text-white transition duration-200 ${
+            currentPage === "/explore" ? "text-white" : ""
           }`}
         >
           Explore
         </Link>
         <Link
           to="/create"
-          className={`text-gray-400 hover:text-white ${
-            currentPage === "/create" ? "text-white-400 font-bold" : ""
+          className={`text-gray-400 hover:text-white transition duration-200 ${
+            currentPage === "/create" ? "text-white" : ""
           }`}
         >
           Create
         </Link>
         <Link
           to="/my-quotes"
-          className={`text-gray-400 hover:text-white ${
-            currentPage === "/my-quotes" ? "text-white-400 font-bold" : ""
+          className={`text-gray-400 hover:text-white transition duration-200 ${
+            currentPage === "/my-quotes" ? "text-white" : ""
           }`}
         >
           My Quotes
         </Link>
         <Link
           to="/support"
-          className={`text-gray-400 hover:text-white ${
-            currentPage === "/support" ? "text-white-400 font-bold" : ""
+          className={`text-gray-400 hover:text-white transition duration-200 ${
+            currentPage === "/support" ? "text-white" : ""
           }`}
         >
           Support
