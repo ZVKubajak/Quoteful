5807753743063769a8bd230546de91dca643d314
VanceGoblin <zvkubajak@gmail.com>
2025-01-14 18:59:49 -0600

    Added Heading Section to Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 const Home = () => {
   return (
     <main>
-      <h1>Hello World</h1>
+      <section className="heading">
+        <h1>Quoteful</h1>
+        <p>Explore and create your own thoughtful quotes.</p>
+
+        <div className="heading-cta-buttons">
+          <button>Explore</button>
+          <button>Create a Quote</button>
+        </div>
+      </section>
     </main>
   );
 };
