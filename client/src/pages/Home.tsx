4798682e68bb9d64708c4fde88f84bbb1f00af31
VanceGoblin <zvkubajak@gmail.com>
2025-01-14 21:59:54 -0600

    Finished Home.tsx Heading

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,15 +3,19 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
     <main>
-      <section id="heading">
-        <h1 className="font-mono">Quoteful</h1>
-        <p className="font-caveat">
+      <section id="heading" className="text-center">
+        <h1 className="font-mono text-8xl py-20">Quoteful</h1>
+        <p className="font-caveat text-7xl pb-40">
           Explore and create your own thoughtful quotes.
         </p>
 
-        <div id="heading-cta-buttons">
-          <Button>Explore</Button>
-          <button className="bg-red-500 text-white p-4">Create a Quote</button>
+        <div id="heading-cta-buttons" className="space-x-4">
+          <Button variant="outline" className="bg-neutral-950">
+            Explore
+          </Button>
+          <Button variant="outline" className="bg-neutral-950">
+            Create a Quote
+          </Button>
         </div>
       </section>
     </main>
