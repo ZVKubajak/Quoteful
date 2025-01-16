68426a388a9472e0ec6e56108f0a275d3bcf706b
VanceGoblin <zvkubajak@gmail.com>
2025-01-16 08:06:01 -0600

    Created Form Login For Signup.tsx

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -19,8 +19,49 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+const signUpFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, {
+      message: "Username must be at least 3 characters.",
+    })
+    .max(20, {
+      message: "Username can not be more than 20 characters.",
+    }),
+  email: z.string().refine((value) => validator.isEmail(value), {
+    message: "Invalid email address.",
+  }),
+  password: z.string().refine((value) => validator.isStrongPassword(value), {
+    message:
+      "Password must be 8-20 characters long, include at least one lowercase letter, one uppercase letter, one number, and one special character.",
+  }),
+});
+
 const SignUp = () => {
   const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof signUpFormSchema>>({
+    resolver: zodResolver(signUpFormSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof signUpFormSchema>) => {
+    try {
+      const { token } = await signUp(values);
+      auth.login(token);
+
+      navigate("/");
+    } catch (errorMessage: any) {
+      console.error("Signup Failed:", errorMessage);
+      form.setError("root", {
+        message: "Unable to create account, please try again.",
+      });
+    }
+  };
 
   return (
     <main>
