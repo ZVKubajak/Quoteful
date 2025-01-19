de0a6274406e8a101a49c094006d14c296ec5ddd
VanceGoblin <zvkubajak@gmail.com>
2025-01-19 10:24:49 -0600

    Created Scroll Area For Quotes

--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -12,6 +12,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type Tag = keyof typeof tagStyles;
 
@@ -59,18 +61,127 @@ const Explore = () => {
         </div>
       </div>
 
-      <div id="explore-container" className="flex border h-[700px] mx-40 my-16">
-        <div id="container-left" className="w-1/2 border"></div>
-        <div id="container-right" className="w-1/2 border">
-          <div id="container-right-top" className="h-1/2 border"></div>
-          <div id="container-right-bottom" className="flex h-1/2 border">
+      <div
+        id="explore-container"
+        className="flex h-[700px] mx-40 my-16 bg-neutral-950/50"
+      >
+        <div
+          id="container-left"
+          className="flex flex-col w-1/2 border rounded-l-xl"
+        >
+          <div id="current-search" className="border-b border-gray-400">
+            <p className="text-4xl p-4">Quotes by:</p>
+          </div>
+          <ScrollArea className="flex-grow px-20">
+            <div className="space-y-20 py-12">
+              <div
+                id="searched-quote-container"
+                className="border rounded-2xl text-xl p-4"
+              >
+                <p className="text-clip overflow-hidden">
+                  "What's up guys, this is a quote."
+                </p>
+                <div className="flex mt-10 mx-4">
+                  <div className="flex w-3/5">
+                    <h2 className="text-2xl">– Username</h2>
+                  </div>
+
+                  {tag && (
+                    <div className="flex justify-end w-2/5">
+                      <Badge
+                        className={`border-2 rounded-lg text-lg ${tagStyles[tag]}`}
+                      >
+                        {tag}
+                      </Badge>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div
+                id="searched-quote-container"
+                className="border rounded-2xl text-xl p-4"
+              >
+                <p className="text-clip overflow-hidden">
+                  "What's up guys, this is a quote."
+                </p>
+                <div className="flex mt-10 mx-4">
+                  <div className="flex w-3/5">
+                    <h2 className="text-2xl">– Username</h2>
+                  </div>
+
+                  {tag && (
+                    <div className="flex justify-end w-2/5">
+                      <Badge
+                        className={`border-2 rounded-lg text-lg ${tagStyles[tag]}`}
+                      >
+                        {tag}
+                      </Badge>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div
+                id="searched-quote-container"
+                className="border rounded-2xl text-xl p-4"
+              >
+                <p className="text-clip overflow-hidden">
+                  "What's up guys, this is a quote."
+                </p>
+                <div className="flex mt-10 mx-4">
+                  <div className="flex w-3/5">
+                    <h2 className="text-2xl">– Username</h2>
+                  </div>
+
+                  {tag && (
+                    <div className="flex justify-end w-2/5">
+                      <Badge
+                        className={`border-2 rounded-lg text-lg ${tagStyles[tag]}`}
+                      >
+                        {tag}
+                      </Badge>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div
+                id="searched-quote-container"
+                className="border rounded-2xl text-xl p-4"
+              >
+                <p className="text-clip overflow-hidden">
+                  "What's up guys, this is a quote."
+                </p>
+                <div className="flex mt-10 mx-4">
+                  <div className="flex w-3/5">
+                    <h2 className="text-2xl">– Username</h2>
+                  </div>
+
+                  {tag && (
+                    <div className="flex justify-end w-2/5">
+                      <Badge
+                        className={`border-2 rounded-lg text-lg ${tagStyles[tag]}`}
+                      >
+                        {tag}
+                      </Badge>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </ScrollArea>
+        </div>
+        <div id="container-right" className="w-1/2">
+          <div
+            id="container-right-top"
+            className="h-1/2 border rounded-r-xl"
+          ></div>
+          <div id="container-right-bottom" className="flex h-1/2">
             <div
               id="container-right-bottom-left"
-              className="w-1/2 border"
+              className="w-1/2 border-x border-b rounded-br-lg"
             ></div>
             <div
               id="container-right-bottom-right"
-              className="w-1/2 border"
+              className="w-1/2 bg-[#02040D]"
             ></div>
           </div>
         </div>
