7a177e28e170c50c4cf176ae91538ca16cd6951c
VanceGoblin <zvkubajak@gmail.com>
2025-01-20 13:43:03 -0600

    Created Starting Logic

--- client/src/pages/MyQuotes.tsx
+++ client/src/pages/MyQuotes.tsx
@@ -7,12 +7,26 @@ import {
   deleteQuote,
 } from "@/services/quoteService";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 import Swal from "sweetalert2";
 import tagStyles from "@/lib/tagStyles";
 import { Pencil } from "lucide-react";
 import { Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import {
   Select,
   SelectContent,
@@ -22,8 +36,99 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
+
+type Tag = keyof typeof tagStyles;
+
+interface Quote {
+  id: string;
+  tag: Tag;
+  content: string;
+  user: {
+    username: string;
+  };
+}
+
+const quoteFormSchema = z.object({
+  quote: z
+    .string()
+    .min(8, {
+      message: "Quote must be at least 8 characters long.",
+    })
+    .max(200, {
+      message: "Quote can not be more than 200 characters.",
+    }),
+  tag: z.string().optional(),
+});
 
 const MyQuotes = () => {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [quoteId, setQuoteId] = useState<string>("");
+  const [content, setContent] = useState("");
+  const [tag, setTag] = useState<Tag | "">("");
+  const [query, setQuery] = useState<string>("");
+
+  const queryCharCount = 200;
+  const contentCharCount = 200;
+
+  const navigate = useNavigate();
+
+  let userId = "";
+  let username = "";
+  if (!auth.guestLoggedIn()) {
+    const profile = auth.getProfile();
+    if (profile) {
+      (userId = profile.id), (username = profile.username);
+    }
+  } else {
+    Swal.fire({
+      title: "Account Required",
+      text: "You need an account to write a quote.",
+      icon: "warning",
+      confirmButtonText: "Create Account",
+      confirmButtonColor: "#3085d6",
+      background: "#333",
+      color: "#fff",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate("/signup");
+      }
+    });
+  }
+
+  const form = useForm<z.infer<typeof quoteFormSchema>>({
+    resolver: zodResolver(quoteFormSchema),
+    defaultValues: {
+      quote: "",
+      tag: "",
+    },
+  });
+
+  // const onSubmit = async (values: z.infer<typeof quoteFormSchema>) => {
+  //   try {
+  //     await updateQuote(
+  //       `"${values.quote}"`,
+  //       values.tag || ""
+  //     );
+
+  //     Swal.fire({
+  //       title: "Quote Updated",
+  //       icon: "success",
+  //       confirmButtonText: "Continue",
+  //       confirmButtonColor: "#3085d6",
+  //       background: "#333",
+  //       color: "#fff",
+  //       allowOutsideClick: false,
+  //       allowEscapeKey: false,
+  //     }).then((result) => {
+  //       if (result.isConfirmed) {
+  //       }
+  //     });
+  //   } catch (error) {}
+  // };
+
   return (
     <main className="flex bg-red-950/25 h-screen">
       <div className="flex flex-col h-screen w-1/2 border">
