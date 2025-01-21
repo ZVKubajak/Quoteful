85dfc0c676790c27b679ab59bc7874809df478d2
VanceGoblin <zvkubajak@gmail.com>
2025-01-21 08:01:18 -0600

    Created updateUser Function

---
+++ client/src/services/userService.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const updateUser = async (userId: string, username: string) => {
+  try {
+    const response = await axios.put(`http://localhost:3001/user/${userId}`, {
+      username,
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    throw error;
+  }
+};
