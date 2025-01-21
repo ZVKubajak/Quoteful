c3b2c1461d462c5d8f56a191dd266f9934bb3020
VanceGoblin <zvkubajak@gmail.com>
2025-01-21 07:57:22 -0600

    Created Basic Layout For Support

--- client/src/pages/Support.tsx
+++ client/src/pages/Support.tsx
@@ -1,5 +1,12 @@
+import { Separator } from "@/components/ui/separator";
+
 const Support = () => {
-  return <main></main>;
+  return (
+    <main className="h-screen border">
+      <h1 className="text-center text-6xl mt-20">Support</h1>
+      <section></section>
+    </main>
+  );
 };
 
 export default Support;
