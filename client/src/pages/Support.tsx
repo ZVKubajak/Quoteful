c12feceb8eb4c6c01ee0de7859110c978b296224
VanceGoblin <zvkubajak@gmail.com>
2025-01-21 13:31:03 -0600

    Added Some Logic to Support.tsx

--- client/src/pages/Support.tsx
+++ client/src/pages/Support.tsx
@@ -1,9 +1,11 @@
 import auth from "@/auth/auth";
 import { updateUsername } from "@/services/userService";
+import { useState, useEffect } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
@@ -18,10 +20,57 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
+const usernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, {
+      message: "Username must be at least 3 characters.",
+    })
+    .max(20),
+});
+
 const Support = () => {
+  const [selected, setSelected] = useState<
+    "update" | "delete" | "feedback" | null
+  >(null);
+
+  const navigate = useNavigate();
+
+  let userId = "";
+  let username = "";
+  if (!auth.guestLoggedIn()) {
+    const profile = auth.getProfile();
+    if (profile) {
+      (userId = profile.id), (username = profile.username);
+    }
+  } else {
+    Swal.fire({
+      title: "Account Required",
+      text: "You need an account to write a quote.",
+      icon: "warning",
+      confirmButtonText: "Create Account",
+      confirmButtonColor: "#3085d6",
+      background: "#333",
+      color: "#fff",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate("/signup");
+      }
+    });
+  }
+
+  const form = useForm<z.infer<typeof usernameFormSchema>>({
+    resolver: zodResolver(usernameFormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
   return (
-    <main className="h-screen border">
-      <div className="">
+    <main className="h-screen">
+      <section id="support-nav">
         <h1 className="font-montserrat text-center text-5xl mt-20">Support</h1>
         <Separator className="bg-gray-600 mx-auto mt-6 mb-4 w-96" />
         <div className="flex h-5 justify-center space-x-4">
@@ -37,7 +86,7 @@ const Support = () => {
             Feedback
           </p>
         </div>
-      </div>
+      </section>
       <section></section>
     </main>
   );
