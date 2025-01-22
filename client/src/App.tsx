e4cddd3cc3f41c060b7d625c2f40522fca298ca9
VanceGoblin <zvkubajak@gmail.com>
2025-01-22 08:23:58 -0600

    Created Footer.tsx & Connected it to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./layout/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
